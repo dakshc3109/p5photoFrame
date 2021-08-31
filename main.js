6bc06b01e6b76c4f709eb814b84ae5f090060d6e
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-08-31 18:47:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var video; 
+function preload(){
+
+}
+
+function setup(){
+    var canvas = createCanvas(1000, 600);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+   
+
+
+    fill(232, 159, 245);
+    stroke(227, 104, 104);
+    circle(90, 500, 80, 80);
+
+    fill(225, 255, 74);
+    stroke(225, 255, 74);
+    rect(130, 480, 750, 40);
+
+    fill(232, 159, 245);
+    stroke(227, 104, 104);
+    circle(920, 500, 80, 80);
+
+    
+    fill(232, 159, 245);
+    stroke(227, 104, 104);
+    circle(90, 100, 80, 80);
+
+    fill(225, 255, 74);
+    stroke(225, 255, 74);
+    rect(130, 80, 750, 40);
+
+    fill(232, 159, 245);
+    stroke(227, 104, 104);
+    circle(920, 100, 80, 80);
+
+    
+    fill(225, 255, 74);
+    stroke(225, 255, 74);
+    rect(72, 140, 40, 320);
+
+    fill(225, 255, 74);
+    stroke(225, 255, 74);
+    rect(902, 140, 40, 320);
+}
+
+function draw(){
+    image(video, 0, 0, 640, 480);
+}
